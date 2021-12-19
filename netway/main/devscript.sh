3befca20cf9c5ef250c0dffe82bc56e446474e58
Arnut Klamhui <arnut@netway.co.th>
2021-12-19 17:34:31 +0700

    use ssl cert file form client.netway.co.th

--- netway/main/devscript.sh
+++ netway/main/devscript.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
+
+#patch license
 cp -a /var/www/domains/netwaymain/billing.netway.co.th/patch/public_html/hbf/components/apiutils/class.apistub.php /var/www/domains/netwaymain/billing.netway.co.th/public_html/hbf/components/apiutils/;
+
 cd /var/www/domains/netwaymain/billing.netway.co.th;
+#skip patch file
 git update-index --skip-worktree public_html/hbf/components/apiutils/class.apistub.php;
+
+#migrate
 php /var/www/domains/netwaymain/billing.netway.co.th/migrate/migrate_script.php;
+
+#htaccess
 if [ ! -L "/var/www/domains/netwaymain/billing.netway.co.th/public_html/.htaccess" ];
 then
     ln -s /var/www/domains/netwaymain/billing.netway.co.th/public_html/.htaccess.develop /var/www/domains/netwaymain/billing.netway.co.th/public_html/.htaccess;
 fi
 
+#install pm2 global
 npm install --prefix /var/www/domains/netwaymain/billing.netway.co.th
 npm install pm2@latest -g
 
+#start nodejs frontend-server
 if [ -d "/var/www/domains/netwaymain/billing.netway.co.th/frontend-server" ];
 then
-cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/billing.netway.co.th.main-key.pem /var/www/domains/netwaymain/billing.netway.co.th/frontend-server/ssl/;
-cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/billing.netway.co.th.main.pem /var/www/domains/netwaymain/billing.netway.co.th/frontend-server/ssl/;
+cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/client.netway.co.th.main-key.pem /var/www/domains/netwaymain/billing.netway.co.th/frontend-server/ssl/;
+cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/client.netway.co.th.main.pem /var/www/domains/netwaymain/billing.netway.co.th/frontend-server/ssl/;
 
 npm install --prefix /var/www/domains/netwaymain/billing.netway.co.th/frontend-server
 pm2 start "npm run dev --prefix /var/www/domains/netwaymain/billing.netway.co.th/frontend-server" --name frontend-server
-
 fi
 
+#start nodejs nova
 if [ -d "/var/www/domains/netwaymain/billing.netway.co.th/hypernova-server" ];
 then
-
-cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/billing.netway.co.th.main-key.pem /var/www/domains/netwaymain/billing.netway.co.th/hypernova-server/src/ssl/;
-cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/billing.netway.co.th.main.pem /var/www/domains/netwaymain/billing.netway.co.th/hypernova-server/src/ssl/;
+cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/client.netway.co.th.main-key.pem /var/www/domains/netwaymain/billing.netway.co.th/hypernova-server/src/ssl/;
+cp -a /var/www/gitworks/laradock-multisite/apache2/local-ssl-certs/client.netway.co.th.main.pem /var/www/domains/netwaymain/billing.netway.co.th/hypernova-server/src/ssl/;
 
 npm install --prefix /var/www/domains/netwaymain/billing.netway.co.th/hypernova-server
 pm2 start "npm run dev --prefix /var/www/domains/netwaymain/billing.netway.co.th/hypernova-server" --name hypernove-server
-
 fi
